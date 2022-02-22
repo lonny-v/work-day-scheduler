95189f492e5fa4c20663d0ab9a995c507a9f5a97
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-02-21 17:30:19 -0700

    change colors based on current time, update local storage when content changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+var button = $("button");
+
 // Display current date and time at top of screen
 $("#currentDay").text(moment().format("dddd") + ", " + moment().format("MMMM Do YYYY, h:mm a"));
 
@@ -14,11 +16,10 @@ function loadItems () {
     $("#text-area-17").val(localStorage.getItem("5PM"));
 };
 
-loadItems();
 // Save all input on the screen whenever any save button is clicked
-$("button").on("click", function (event){
-    event.preventDefault();
-    
+button.on("click", setItems)
+
+function setItems () {
     localStorage.setItem("9AM", $("#text-area-9").val());
     localStorage.setItem("10AM", $("#text-area-10").val());
     localStorage.setItem("11AM", $("#text-area-11").val());
@@ -28,4 +29,35 @@ $("button").on("click", function (event){
     localStorage.setItem("3PM", $("#text-area-15").val());
     localStorage.setItem("4PM", $("#text-area-16").val());
     localStorage.setItem("5PM", $("#text-area-17").val());
-});
+}
+
+// update local storage when content is changed
+$(".time-slot").on("blur", "textarea", function () {
+setItems()
+})
+
+// change time slot color based on time of day
+function setColor () {
+    let currentTime = parseInt(moment().format("HH"));
+    $(".time-slot").each(function (){
+        // check against id for each time slot
+        var timeSlot = $(this).attr("id");
+        // id for each time slot is equivalent in military time
+
+        if (timeSlot < currentTime) {
+            $(this).addClass("past");
+        }
+        else if ( timeSlot == currentTime){
+        $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("future");
+        }
+    });
+}
+
+// check time every 15 mins to reapply colors
+setInterval(setColor(), 900000);
+
+loadItems();
+setColor();
